33ea485f981fa73b730b375a0d0d240673704bd2
Mohit Goenka <mgoenka@yahoo-inc.com>
2014-03-16 18:13:58 -0700

    Logs location in a file

--- src/com/mgoenka/locationlogger/LocationLoggerActivity.java
+++ src/com/mgoenka/locationlogger/LocationLoggerActivity.java
@@ -1,22 +1,211 @@
 package com.mgoenka.locationlogger;
 
-import android.os.Bundle;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.Executors;
+import java.util.concurrent.TimeUnit;
+
 import android.app.Activity;
-import android.view.Menu;
+import android.content.Context;
+import android.graphics.Color;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.TextView;
 
 public class LocationLoggerActivity extends Activity {
+	private static final long MEASURE_TIME = 1000 * 30;
+	private static final long POLLING_FREQ = 1000 * 1;
+	private static final float MIN_DISTANCE = 0.5f;
+	
+
+	// Views for display location information
+	private TextView mAccuracyView;
+	private TextView mTimeView;
+	private TextView mLatView;
+	private TextView mLngView;
+
+	private int mTextViewColor = Color.GRAY;
+
+	// Current best location estimate
+	private Location mBestReading;
+
+	// Reference to the LocationManager and LocationListener
+	private LocationManager mLocationManager;
+	private LocationListener mLocationListener;
+
+	private final String TAG = "Edx";
+
+	private boolean mFirstUpdate = true;
 
 	@Override
-	protected void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
+
 		setContentView(R.layout.activity_location_logger);
+
+		mAccuracyView = (TextView) findViewById(R.id.accuracy_view);
+		mTimeView = (TextView) findViewById(R.id.time_view);
+		mLatView = (TextView) findViewById(R.id.lat_view);
+		mLngView = (TextView) findViewById(R.id.lng_view);
+
+		// Acquire reference to the LocationManager
+		if (null == (mLocationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE)))
+			finish();
+
+		// Get best last location measurement
+		mBestReading = lastKnownLocation();
+
+		// Display last reading information
+		if (null != mBestReading) {
+			updateDisplay(mBestReading);
+		} else {
+			mAccuracyView.setText("No Initial Reading Available");
+		}
+
+		mLocationListener = new LocationListener() {
+			// Called back when location changes
+			public void onLocationChanged(Location location) {
+				ensureColor();
+				// Determine whether new location is better than current best
+				// estimate
+				if (null == mBestReading
+						|| location.getAccuracy() < mBestReading.getAccuracy()) {
+
+					// Update best estimate
+					mBestReading = location;
+
+					// Update display
+					updateDisplay(location);
+				}
+			}
+
+			public void onStatusChanged(String provider, int status,
+					Bundle extras) {
+				// NA
+			}
+
+			public void onProviderEnabled(String provider) {
+				// NA
+			}
+
+			public void onProviderDisabled(String provider) {
+				// NA
+			}
+		};
 	}
 
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.location_logger, menu);
-		return true;
+	protected void onResume() {
+		super.onResume();
+		// Determine whether initial reading is
+		// "good enough"
+		// Register for network location updates
+		mLocationManager.requestLocationUpdates(
+				LocationManager.NETWORK_PROVIDER, POLLING_FREQ, MIN_DISTANCE,
+				mLocationListener);
+
+		// Register for GPS location updates
+		mLocationManager.requestLocationUpdates(
+				LocationManager.GPS_PROVIDER, POLLING_FREQ, MIN_DISTANCE,
+				mLocationListener);
+
+		// Schedule a runnable to unregister location listeners
+		Executors.newScheduledThreadPool(1).schedule(new Runnable() {
+
+			@Override
+			public void run() {
+				Log.i(TAG, "location updates cancelled");
+				mLocationManager.removeUpdates(mLocationListener);
+			}
+		}, MEASURE_TIME, TimeUnit.MILLISECONDS);
 	}
 
+	// Unregister location listeners
+	@Override
+	protected void onPause() {
+		super.onPause();
+		mLocationManager.removeUpdates(mLocationListener);
+	}
+
+	// Get the last known location from all providers
+	// return best reading is as accurate as minAccuracy and
+	// was taken no longer then minTime milliseconds ago
+
+	private Location lastKnownLocation() {
+		Location bestResult = null;
+
+		List<String> matchingProviders = mLocationManager.getAllProviders();
+
+		for (String provider : matchingProviders) {
+			Location location = mLocationManager.getLastKnownLocation(provider);
+			if (location != null) {
+				bestResult = location;
+			}
+		}
+		return bestResult;
+	}
+
+	// Update display
+	private void updateDisplay(Location location) {
+		Float accuracy = location.getAccuracy();
+		String time = new SimpleDateFormat("MM/dd/yyyy HH:mm:ss",
+				Locale.getDefault()).format(new Date(location.getTime()));
+		Double longitude = location.getLongitude();
+		Double latitude = location.getLatitude();
+		
+		appendLog("Accuracy: " + accuracy + ", Time: " + time + ", Longitude: " + longitude + ", Latitude: " +
+				latitude);
+		
+		mAccuracyView.setText("Accuracy:" + accuracy);
+		mTimeView.setText("Time:" + time);
+		mLatView.setText("Longitude:" + longitude);
+		mLngView.setText("Latitude:" + latitude);
+	}
+	
+	public void appendLog(String text) {       
+	   File logFile = new File("/sdcard/edx.log");
+	   if (!logFile.exists()) {
+	      try {
+	         logFile.createNewFile();
+	      } 
+	      catch (IOException e) {
+	         // TODO Auto-generated catch block
+	         e.printStackTrace();
+	      }
+	   }
+	   try {
+	      //BufferedWriter for performance, true to set append to file flag
+	      BufferedWriter buf = new BufferedWriter(new FileWriter(logFile, true)); 
+	      buf.append(text);
+	      buf.newLine();
+	      buf.close();
+	   }
+	   catch (IOException e) {
+	      // TODO Auto-generated catch block
+	      e.printStackTrace();
+	   }
+	}
+
+	private void ensureColor() {
+		if (mFirstUpdate) {
+			setTextViewColor(mTextViewColor);
+			mFirstUpdate = false;
+		}
+	}
+
+	private void setTextViewColor(int color) {
+		mAccuracyView.setTextColor(color);
+		mTimeView.setTextColor(color);
+		mLatView.setTextColor(color);
+		mLngView.setTextColor(color);
+	}
 }
